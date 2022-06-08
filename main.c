9edf80ee3a4598d53718c6d1e9a2b41a346d7bec
Sudo_Odus <104328889+sudo-odus1@users.noreply.github.com>
2022-06-08 18:10:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
+include <stdio.h>
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("School\n");
+    printf("Hello, School\n");
   }
   return 0;
 }
